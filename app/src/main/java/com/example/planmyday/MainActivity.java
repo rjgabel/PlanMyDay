e24e66f6f32a10886c0ea6fd9abf594abb66472e
Tyler Tran <tntran@usc.edu>
2023-10-16 23:48:14 -0700

    Added firebase dependencies

--- app/src/main/java/com/example/planmyday/MainActivity.java
+++ app/src/main/java/com/example/planmyday/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.planmyday;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/*
+HOME PAGE
+Contains login and sign up
+ */
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseDatabase db = FirebaseDatabase.getInstance("https://planmyday-16506-default-rtdb.firebaseio.com/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //create onClickListener for login/signup
+
     }
+
+    //TODO: Change return type to UserAccount
+    private void login(TextView email, TextView password){
+
+    }
+
+    //TODO: Change return type to UserAccount
+    private void signUp(TextView name, TextView email, TextView password){
+
+    }
+
 }
