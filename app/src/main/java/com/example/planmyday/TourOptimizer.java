4f9f461f36b5a528ccad3829cdba2140457cdca8
Ryan Gabel <rjgabel@usc.edu>
2023-10-20 14:39:33 -0700

    Finish TourOptimizer

--- app/src/main/java/com/example/planmyday/TourOptimizer.java
+++ app/src/main/java/com/example/planmyday/TourOptimizer.java
@@ -10,10 +10,50 @@ import java.util.ArrayList;
 public class TourOptimizer {
     public static ArrayList<TourPlan> optimizeTour(ArrayList<Attraction> attractions) {
         ArrayList<TourStop> stops = new ArrayList<>();
-        for (Attraction attraction : attractions) {
-            stops.add(new TourStop(attraction, attraction.getHours().get(0).get(0), attraction.getHours().get(0).get(1)));
-        }
         ArrayList<TourPlan> plans = new ArrayList<>();
+
+        int cur_day = 0;
+        int cur_time = 0;
+        while (!attractions.isEmpty()) {
+            int best_attraction = -1;
+            int best_arrival_time = -1;
+            int best_departure_time = -1;
+            for (int i = 0; i < attractions.size(); i++) {
+                Attraction attraction = attractions.get(i);
+                int open_time = attraction.getHours().get(cur_day).get(0);
+                int close_time = attraction.getHours().get(cur_day).get(1);
+                int arrival_time = cur_time;
+                if (arrival_time < open_time) {
+                    arrival_time = open_time;
+                }
+                int departure_time = open_time + attraction.getTime();
+                if (departure_time > close_time) {
+                    // Can't make it to this attraction today
+                    continue;
+                }
+                if (best_arrival_time == -1 || arrival_time < best_arrival_time) {
+                    best_attraction = i;
+                    best_arrival_time = arrival_time;
+                    best_departure_time = departure_time;
+                }
+            }
+            if (best_attraction != -1) {
+                stops.add(new TourStop(attractions.get(best_attraction), best_arrival_time, best_departure_time));
+                attractions.remove(best_attraction);
+                cur_time = best_departure_time;
+            } else {
+                // We can't visit any more attractions today, go to next
+                cur_day += 1;
+                if (cur_day >= 7) {
+                    // Can our thing even last more than one week? Fix this if needed!
+                    Log.d("TourOptimizer", "Going to crash now, check TourOptimizer for info");
+                    System.exit(-1);
+                }
+                cur_time = 0;
+                plans.add(new TourPlan(stops));
+                stops = new ArrayList<>();
+            }
+        }
         plans.add(new TourPlan(stops));
         return plans;
     }
