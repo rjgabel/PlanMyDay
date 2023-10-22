0d77ddc90b24239dc6801dbdd8d1dedb20c5d171
Tyler Tran <tntran@usc.edu>
2023-10-22 16:55:46 -0700

    added google maps dependencies

--- app/src/main/java/com/example/planmyday/map/ItineraryActivity.java
+++ app/src/main/java/com/example/planmyday/map/ItineraryActivity.java
@@ -1,15 +1,38 @@
 package com.example.planmyday.map;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.planmyday.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ItineraryActivity extends AppCompatActivity {
 
+
+public class ItineraryActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerary);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
--- app/src/main/java/com/example/planmyday/planning/DurationActivity.java
+++ app/src/main/java/com/example/planmyday/planning/DurationActivity.java
@@ -1,26 +1,43 @@
 package com.example.planmyday.planning;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.planmyday.R;
+import com.example.planmyday.map.ItineraryActivity;
 
 
 public class DurationActivity extends AppCompatActivity {
 
+    Button reviewItineraryButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duration);
 
         ImageView arrow = findViewById(R.id.arrow);
+        reviewItineraryButton = findViewById(R.id.reviewItineraryButton);
+        reviewItineraryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toMap();
+            }
+        });
 
         arrow.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 onBackPressed();
             }
         });
+    }
+
+    private void toMap(){
+        Intent intent = new Intent(this, ItineraryActivity.class);
+        startActivity(intent);
     }
 }
