24416bef4ca5c0571f6f6131d8b4d18fec34581a
Samantha Sung <sysung@usc.edu>
2023-10-21 20:26:25 -0700

    froggy

--- app/src/main/java/com/example/planmyday/activities/CreateAttractions.java
+++ app/src/main/java/com/example/planmyday/activities/CreateAttractions.java
@@ -51,6 +51,7 @@ public class CreateAttractions {
 
         } catch (IOException e){
             e.printStackTrace();
+            Log.d("SAMCHECK", e.getMessage());
         }
 
         Gson gs = new Gson();
