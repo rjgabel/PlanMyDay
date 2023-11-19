0d6bd041a5fe9711d970e44d7340300eed9142d0
Ryan Gabel <rjgabel@usc.edu>
2023-11-19 12:53:35 -0800

    Add test

---
+++ app/src/androidTest/java/com/example/planmyday/home/RyanInstrumentedTestLABlank.java
@@ -0,0 +1,84 @@
+package com.example.planmyday.home;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.example.planmyday.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RyanInstrumentedTestLABlank {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    private static Matcher<View> childAtPosition(final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent) && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+    @Test
+    public void ryanInstrumentedTestLABlank() {
+        ViewInteraction appCompatButton = onView(allOf(withId(R.id.loginButton), withText("LOG IN"), childAtPosition(childAtPosition(withId(android.R.id.content), 0), 2), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction textInputEditText = onView(allOf(withId(R.id.email), childAtPosition(childAtPosition(withId(android.R.id.content), 0), 2), isDisplayed()));
+        textInputEditText.perform(replaceText("rjgabel@usc.edu"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText2 = onView(allOf(withId(R.id.password), childAtPosition(childAtPosition(withId(android.R.id.content), 0), 3), isDisplayed()));
+        textInputEditText2.perform(replaceText("rjgabel"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton2 = onView(allOf(withId(R.id.btn_login), withText("LOG IN"), childAtPosition(childAtPosition(withId(android.R.id.content), 0), 4), isDisplayed()));
+        appCompatButton2.perform(click());
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        ViewInteraction appCompatButton3 = onView(allOf(withId(R.id.newitinerary), withText("+"), childAtPosition(childAtPosition(withId(android.R.id.content), 0), 3), isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatImageView = onView(allOf(withId(R.id.laButton), childAtPosition(childAtPosition(withId(android.R.id.content), 0), 2), isDisplayed()));
+        appCompatImageView.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(allOf(withId(R.id.nextButton), withText("NEXT"), childAtPosition(childAtPosition(withId(android.R.id.content), 0), 0), isDisplayed()));
+        appCompatButton4.perform(click());
+    }
+}
